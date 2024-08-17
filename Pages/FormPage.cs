21fd8d800dd2c1b0a20514d093fd2a1a5eb19d55
paulius.znk <paulius.znk@gmail.com>
2024-08-17 14:04:00 +0300

    Implemented test using POM structure

---
+++ Pages/FormPage.cs
@@ -0,0 +1,49 @@
+﻿using InputFormTests.Models;
+using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputFormTests.Pages
+{
+    public class FormPage
+    {
+        private readonly IPage _page;
+        private readonly ILocator _firstNameInput;
+        private readonly ILocator _lastNameInput;
+        private readonly ILocator _ageInput;
+        private readonly ILocator _countryInput;
+        private readonly ILocator _notesInput;
+        private readonly ILocator _submitButton;
+        private readonly ILocator _firstNameCustomError;
+        private readonly ILocator _firstNameHtmlError;
+
+
+        public FormPage(IPage page)
+        {
+            _page = page;
+            _firstNameInput = _page.Locator("#firstname");
+            _lastNameInput = _page.Locator("#surname");
+            _ageInput = _page.Locator("#age");
+            _countryInput = _page.Locator("#country");
+            _notesInput = _page.Locator("#notes");
+            _submitButton = _page.Locator("input[type=\"submit\"]");
+            _firstNameCustomError = _page.Locator("[name=\"firstnamevalidation\"]");
+        }
+        public async Task GoTo()
+        {
+            await _page.GotoAsync("https://testpages.eviltester.com/styled/validation/input-validation.html");
+        }
+        public async Task SubmitForm(Submission submission)
+        {
+            await _firstNameInput.FillAsync(submission.FirstName);
+            await _lastNameInput.FillAsync(submission.SecondName);
+            await _ageInput.FillAsync(submission.Age.ToString());
+            await _countryInput.SelectOptionAsync(submission.Country);
+            await _notesInput.FillAsync(submission.Notes);
+            await _submitButton.ClickAsync();
+        }
+    }
+}
---
+++ Tests/FormTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Playwright.NUnit;
+using Microsoft.Playwright;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using InputFormTests.Pages;
+using InputFormTests.Utilities;
+using InputFormTests.Models;
+
+namespace InputFormTests.Tests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class FormTests : PageTest
+{
+    private FormPage _formPage;
+    private ResponsePage _responsePage;
+    [SetUp]
+    public void SetUp()
+    {
+        _formPage = new FormPage(Page);
+        _responsePage = new ResponsePage(Page);
+    }
+
+    [Test]
+    public async Task Submit()
+    {
+        Submission submission = new Submission("asdasdasdasdasd", "asdasdasdasdasd", 18, "Lithuania", "");
+        await _formPage.GoTo();
+        await _formPage.SubmitForm(submission);
+        await Task.Delay(3000);
+        await _responsePage.CheckResponse(submission);
+    }
+        //    [Test]
+        //    public async Task GetStartedLink()
+        //    {
+        //        await Page.GotoAsync("https://playwright.dev");
+
+        //        // Click the get started link.
+        //        await Page.GetByRole(AriaRole.Link, new() { Name = "Get started" }).ClickAsync();
+
+        //        // Expects page to have a heading with the name of Installation.
+        //        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Installation" })).ToBeVisibleAsync();
+        //    }
+    }
