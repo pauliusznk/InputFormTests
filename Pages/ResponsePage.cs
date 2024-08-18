e3d74a81cfd2a3a4a2a43d4a1df5bcd326b3409d
Paulius <74420496+pauliusznk@users.noreply.github.com>
2024-08-18 21:24:02 +0300

    Revert "CS project delete"

---
+++ Pages/ResponsePage.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Playwright.NUnit;
+using InputFormTests.Models;
+
+namespace InputFormTests.Pages
+{
+    public class ResponsePage
+    {
+        private readonly IPage _page;
+        private readonly ILocator _firstNameResponse;
+        private readonly ILocator _secondNameResponse;
+        private readonly ILocator _ageResponse;
+        private readonly ILocator _countryResponse;
+        private readonly ILocator _notesResponse;
+        private readonly ILocator _responseHeader;
+        public ResponsePage(IPage page)
+        {
+            _page = page;
+            _firstNameResponse = _page.Locator("#firstname-value");
+            _secondNameResponse = _page.Locator("#surname-value");
+            _ageResponse = _page.Locator("#age-value");
+            _countryResponse = _page.Locator("#country-value");
+            _notesResponse = _page.Locator("#notes-value");
+            _responseHeader = _page.GetByText("Input Validation Response");
+        }
+        public async Task AssertPageIsNotLoaded()
+        {
+            bool isVisible = await _responseHeader.IsVisibleAsync();
+            Assert.IsFalse(isVisible);
+        }
+        public async Task CheckResponse(Submission submission)
+        {
+            await CheckField(_firstNameResponse, submission.FirstName, "First name");
+            await CheckField(_secondNameResponse, submission.SecondName, "Second name");
+            await CheckField(_ageResponse, submission.Age.ToString(), "Age");
+            await CheckField(_countryResponse, submission.Country, "Country");
+            await CheckField(_notesResponse, submission.Notes, "Notes");
+        }
+        private async Task CheckField(ILocator fieldLocator, string expectedValue, string fieldName)
+        {
+            var actualValue = await fieldLocator.InnerTextAsync();
+            if (actualValue != expectedValue)
+                throw new Exception($"{fieldName} mismatch: expected '{expectedValue}', got '{actualValue}'.");
+        }
+
+    }
+}
---
+++ Tests/FormTests.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Playwright.NUnit;
+using Microsoft.Playwright;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using InputFormTests.Pages;
+using InputFormTests.Utilities;
+using InputFormTests.Models;
+
+namespace InputFormTests.Tests
+{
+    [Parallelizable(ParallelScope.Self)]
+    [TestFixture]
+    public class FormTests : PageTest
+    {
+        private FormPage _formPage;
+        private ResponsePage _responsePage;
+        [SetUp]
+        public void SetUp()
+        {
+            _formPage = new FormPage(Page);
+            _responsePage = new ResponsePage(Page);
+        }
+
+        [Test]
+        public async Task CorrectSubmissionMinimumLengthValues()
+        {
+            Submission submission = new Submission(StringGenerator.GenerateRandomString(1), StringGenerator.GenerateRandomString(11), 18, "Afghanistan", StringGenerator.GenerateRandomString(1));
+            await _formPage.GoTo();
+            await _formPage.SubmitForm(submission);
+            await _responsePage.CheckResponse(submission);
+        }
+        [Test]
+        public async Task CorrectSubmissionMaximumLengthValues()
+        {
+            Submission submission = new Submission(StringGenerator.GenerateRandomString(89), StringGenerator.GenerateRandomString(79), 80, "Zimbabwe", StringGenerator.GenerateRandomString(2000));
+            await _formPage.GoTo();
+            await _formPage.SubmitForm(submission);
+            await _responsePage.CheckResponse(submission);
+        }
+        [Test]
+        public async Task IncorrectSubmissionEmptyFirstName()
+        {
+            Submission submission = new Submission("", StringGenerator.GenerateRandomString(79), 80, "Zimbabwe", StringGenerator.GenerateRandomString(1));
+            await _formPage.GoTo();
+            await _formPage.SubmitForm(submission);
+            await _formPage.FirstNameErrorVisible();
+            await _responsePage.AssertPageIsNotLoaded();
+        }
+        [Test]
+        public async Task IncorrectSubmissionFirstNameTooLong()
+        {
+            Submission submission = new Submission(StringGenerator.GenerateRandomString(90), StringGenerator.GenerateRandomString(79), 80, "Zimbabwe", StringGenerator.GenerateRandomString(1));
+            await _formPage.GoTo();
+            await _formPage.SubmitForm(submission);
+            await _formPage.FirstNameErrorVisible();
+            await _responsePage.AssertPageIsNotLoaded();
+        }
+        [Test]
+        public async Task IncorrectSubmissionEmptyLastName()
+        {
+            Submission submission = new Submission(StringGenerator.GenerateRandomString(20), "", 80, "Zimbabwe", StringGenerator.GenerateRandomString(1));
+            await _formPage.GoTo();
+            await _formPage.SubmitForm(submission);
+            await _formPage.LastNameErrorVisible();
+            await _responsePage.AssertPageIsNotLoaded();
+        }
+        [Test]
+        public async Task IncorrectSubmissionTooShortLastName()
+        {
+            Submission submission = new Submission(StringGenerator.GenerateRandomString(20), StringGenerator.GenerateRandomString(10), 80, "Zimbabwe", StringGenerator.GenerateRandomString(1));
+            await _formPage.GoTo();
+            await _formPage.SubmitForm(submission);
+            await _formPage.LastNameErrorVisible();
+            await _responsePage.AssertPageIsNotLoaded();
+        }
+        [Test]
+        public async Task IncorrectSubmissionTooLongLastName()
+        {
+            Submission submission = new Submission(StringGenerator.GenerateRandomString(20), StringGenerator.GenerateRandomString(80), 80, "Zimbabwe", StringGenerator.GenerateRandomString(1));
+            await _formPage.GoTo();
+            await _formPage.SubmitForm(submission);
+            await _formPage.LastNameErrorVisible();
+            await _responsePage.AssertPageIsNotLoaded();
+        }
+        [Test]
+        public async Task IncorrectSubmissionEmptyAge()
+        {
+            Submission submission = new Submission(StringGenerator.GenerateRandomString(20), StringGenerator.GenerateRandomString(20), null, "Lithuania", StringGenerator.GenerateRandomString(1));
+            await _formPage.GoTo();
+            await _formPage.SubmitForm(submission);
+            await _formPage.AgeErrorVisible();
+            await _responsePage.AssertPageIsNotLoaded();
+        }
+        [Test]
+        public async Task IncorrectSubmissionTooLittleAge()
+        {
+            Submission submission = new Submission(StringGenerator.GenerateRandomString(20), StringGenerator.GenerateRandomString(20), 17, "Lithuania", StringGenerator.GenerateRandomString(1));
+            await _formPage.GoTo();
+            await _formPage.SubmitForm(submission);
+            await _formPage.AgeErrorVisible();
+            await _responsePage.AssertPageIsNotLoaded();
+        }
+        [Test]
+        public async Task IncorrectSubmissionTooBigAge()
+        {
+            Submission submission = new Submission(StringGenerator.GenerateRandomString(20), StringGenerator.GenerateRandomString(20), 81, "Lithuania", StringGenerator.GenerateRandomString(1));
+            await _formPage.GoTo();
+            await _formPage.SubmitForm(submission);
+            await _formPage.AgeErrorVisible();
+            await _responsePage.AssertPageIsNotLoaded();
+        }
+    }
+}
